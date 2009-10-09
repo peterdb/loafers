1af0cfdb4e72f17f868fbe9f32ce826d25260f14
peterdb <peter.de.bruycker@gmail.com>
2009-10-09 13:16:45 +0000

    fixed broken sample
    git-svn-id: http://loafers.googlecode.com/svn/trunk@54 47516a04-069f-11de-b321-f573fdaff03e

--- loafers-examples/src/main/groovy/loafers/samples/TestStylingSample.groovy
+++ loafers-examples/src/main/groovy/loafers/samples/TestStylingSample.groovy
@@ -5,13 +5,13 @@ import loafers.Loafers
 Loafers.app {
 	stack {
 		// Background, text and a button: both are elements!
-		back  = background(green)
-		text  = banner("A Message for You, Rudy")
-		press = button("Stop your messin about!")
+		def back  = background(green)
+		def text  = banner("A Message for You, Rudy")
+		def button = button("Stop your messin about!")
 		
 		// And so, they can be styled.
 		text.style(size:12, stroke:tomato, margin:10)
-		press.style(width:400)
+		button.style(width:400)
 		back.style(height:10)
 	}
 }
